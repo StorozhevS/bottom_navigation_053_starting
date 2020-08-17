7a4b2bad4f6b51066fa85dad5b43306aeffc04c4
s.storozhev <7549892@gmail.com>
2020-08-17 15:58:08 +0300

    Initial commit

---
+++ lib/ui/views/posts_example/posts_viewmodel.dart
@@ -0,0 +1,14 @@
+import 'package:my_app/app/locator.dart';
+import 'package:my_app/datamodels/post.dart';
+import 'package:my_app/services/posts_service.dart';
+import 'package:stacked/stacked.dart';
+
+class PostsViewModel extends FutureViewModel<List<Post>> {
+  final _postsService = locator<PostsService>();
+
+  @override
+  Future<List<Post>> futureToRun() {
+    print('Fetch posts');
+    return _postsService.getPostsForUser(3);
+  }
+}
